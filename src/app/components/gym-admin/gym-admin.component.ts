90bcd64030eac756b6d5d2712f23ae9419935a28
Michael.Crawford <cerealean@me.com>
2017-07-06 12:52:36 -0500

    Added font-awesome attribution in footer, changed gym name icon to a fort, and fixed service provider in the component.

--- src/app/components/gym-admin/gym-admin.component.ts
+++ src/app/components/gym-admin/gym-admin.component.ts
@@ -1,32 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { AuthenticationService } from "../../services/authentication/authentication.service";
+import { DOCUMENT } from "@angular/platform-browser";
 import { User } from "../../models/user";
 import { GymService } from "app/services/gym/gym.service";
 import { Gym } from "app/models/gym";
 
 @Component({
   templateUrl: "./gym-admin.component.html",
-  styleUrls: ["./gym-admin.component.scss"]
+  styleUrls: ["./gym-admin.component.scss"],
+  providers: [GymService]
 })
 export class GymAdminComponent implements OnInit {
+  public document: Document;
   public user: User = this.authenticationService.getCurrentlyLoggedInUser();
-  public gym:Gym;
+  public gym: Gym = new Gym();
 
   constructor(
     private authenticationService: AuthenticationService,
-    private gymService: GymService
-  ) { }
+    private gymService: GymService,
+    @Inject(DOCUMENT) document:any
+  ) { 
+    this.document = <Document> document;
+  }
 
   ngOnInit() {
     this.getGymForUser();
   }
-  
+
+  doFocus(name: string) {
+    const element = this.document.getElementsByName(name)[0];
+    element.scrollIntoView();
+    element.focus();
+  }
+
   private getGymForUser() {
     this.gymService
       .getGymFromUserId(this.user.id)
       .subscribe(
-      (gym: Gym) => this.gym = gym,
-      (error) => alert("There was an error retrieving your gym information")  
+        (gym: Gym) => (this.gym = gym),
+        error => alert("There was an error retrieving your gym information")
       );
   }
 }
